86e10f1f7be47856c34ef0a18f320263b3182ba0
sytniky <sytniky@gmail.com>
2018-06-14 19:26:26 +0300

    Update TryFinally.java

--- src/main/java/TryFinally.java
+++ src/main/java/TryFinally.java
@@ -2,6 +2,7 @@ public class TryFinally {
     public static void main(String[] args) {
         try {
             System.out.println("Test 1");
+            System.out.println("Test 3");
             throw new RuntimeException();
         } finally {
             System.out.println("Test 2");
